16ee9c9b89d5c102666ddad773bd2eadbe889ad2
reirose <dead5root@gmail.com>
2024-03-28 16:16:38 +0300

    added config file

--- bin/init.py
+++ bin/init.py
@@ -1,9 +1,30 @@
+import logging
+
 import sentry_sdk
+
+from json import load
 from fastapi.templating import Jinja2Templates
 
-sentry_sdk.init(
-    dsn="https://e36c13527c6d93485c31205a1309e6da@o4506976349519872.ingest.us.sentry.io/4506976351223808",
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
-)
+newsapi_key = ""
+logfile_path = ""
+
+try:
+    with open("config.json", "rt") as f:
+        cfg = load(f)
+        newsapi_key = cfg["news_api_key"]
+        try:
+            logfile_path = cfg["log_path"]
+        except KeyError:
+            logfile_path = "ntm.log"
+except FileNotFoundError:
+    logging.error("config file not found")
+    input("press [enter] to exit")
+    exit(0)
+
+logging.basicConfig(filename=logfile_path)
+
+sentry_sdk.init(dsn="https://e36c13527c6d93485c31205a1309e6da@o4506976349519872.ingest.us.sentry.io/4506976351223808",
+                traces_sample_rate=1.0,
+                profiles_sample_rate=1.0,)
+
 templates = Jinja2Templates(directory="lib/template")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse
 from bin.db import coll
-from bin.init import templates
+from bin.init import templates, newsapi_key
 from bin.functions import parse_news
 
 app = FastAPI()
@@ -23,13 +23,12 @@ async def trigger_error() -> float:
 
 @app.get("/search", response_class=HTMLResponse)
 async def get_search_query(request: Request, q: Union[str, None] = None) -> templates.TemplateResponse:
-    data = parse_news(f"https://newsapi.org/v2/everything?apiKey=c9530833123349c7a08ead07f0cca6ea"
+    data = parse_news(f"https://newsapi.org/v2/everything?apiKey={newsapi_key}"
                       f"&sortBy=publishedAt&q={q}")
 
     if data['status'] == "ok":
         n = 0
         for item in data["articles"]:
-            print(item)
             coll.insert_one(item)
             n += 1
         data["totalResults"] = n
@@ -44,4 +43,4 @@ async def get_search_query(request: Request, q: Union[str, None] = None) -> temp
 if __name__ == "__main__":
     uvicorn.run(app,
                 host='127.0.0.1',
-                port=8000)
+                port=5000)
